7aedfb6174c0abda0fcdaa1e9973ce4bf228ce96
Arthur0393 <carlos.ara777@gmail.com>
2023-07-12 12:29:30 -0600

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:water_counter_app/screens/start_screen.dart' show StartScreen;
+import 'package:wear_counter_app_1/screens/start_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,22 +12,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Water counter',
+      title: 'Water Counter',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.black,
+        scaffoldBackgroundColor: const Color.fromARGB(83, 0, 0, 0),
         primaryColor: Colors.indigo,
-        textTheme: 
-        const TextTheme(
-            headlineSmall: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            bodyMedium: TextStyle(
-              color: Color.fromARGB(255, 137, 186, 243),
-            )
+        appBarTheme: const AppBarTheme(
+          color: Colors.black,
         ),
+        textTheme: const TextTheme(
+            headlineSmall:
+                TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            bodyMedium: TextStyle(
+              color: Color.fromARGB(195, 10, 237, 245),
+            ),
+            bodySmall: TextStyle(
+                color: Color.fromARGB(255, 219, 151, 5),
+                fontSize: 12,
+                fontWeight: FontWeight.bold)),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.indigo, foregroundColor: Colors.white)),
         useMaterial3: true,
       ),
       home: const StartScreen(),
     );
   }
 }
-
--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,74 +1,149 @@
 import 'package:flutter/material.dart';
-
+import 'package:wear_counter_app_1/assets/interval_progress_bar.dart';
 
 class StartScreen extends StatefulWidget {
-  const StartScreen({super.key});
+  const StartScreen({Key? key}) : super(key: key);
 
   @override
   State<StartScreen> createState() => _StartScreenState();
 }
 
-    
-class _StartScreenState extends State<StartScreen> 
-{
+class _StartScreenState extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
-
-    
-
     return Scaffold(
-      body: Column (
-        mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(
+        toolbarHeight: 10,
+        centerTitle: false,
+        leadingWidth: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Water Counter",
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            Text(
+              "${DateTime.now().hour}:${DateTime.now().minute}",
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ],
+        ),
+        // actions: [],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [_totalMl(), _indicators(), _buttonReg()],
+      ),
+    );
+  }
+
+  Widget _totalMl() {
+    return Column(
+      children: [
+        Text(
+          '0 ml',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        Text(
+          'Faltan 2001 ml',
+          style: Theme.of(context).textTheme.bodyMedium,
+        )
+      ],
+    );
+  }
+
+  Widget _indicators() {
+    return IntrinsicHeight(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          _totalMl(),
-          _indicators(),
-          _buttonReg(),
+          _percentage(),
+          _smallDivider(),
+          _hydration(),
         ],
       ),
     );
   }
 
-  Widget _totalMl(){
-
-       return Column(
-         children: [
-           Text(
-                '0 mL',
-                style: Theme.of(context).textTheme.headlineSmall,
-                ),
-            Text(
-              'Faltan 2500 mL',
-              style: Theme.of(context).textTheme.bodyMedium,
-            )
-         ],
-       );
-
-
-  }
-  Widget _indicators(){
-    return Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                '0 %',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              Text(
-                '1.0',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-            ],
-          );
-  }
-
-  Widget _buttonReg(){
-    return Center(
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-              ), 
-            onPressed: () {},
-            child: const Text('Registrar'),
+  Widget _percentage() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SizedBox(
+            child: Stack(
+          alignment: Alignment.center,
+          children: [
+            const CircularProgressIndicator(
+              strokeWidth: 6,
+              value: .9,
+              color: Color.fromARGB(255, 106, 255, 7),
+              backgroundColor: Color.fromARGB(58, 88, 88, 88),
             ),
-          );
+            Text(
+              '90%',
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          ],
+        )),
+        Text(
+          DateTime.now().toString().split(' ')[0],
+          style: Theme.of(context).textTheme.bodySmall,
+        )
+      ],
+    );
+  }
+
+  Widget _smallDivider() {
+    return const VerticalDivider(
+      color: Color.fromARGB(255, 0, 253, 13),
+      width: 20,
+      thickness: 1,
+      indent: 10,
+      endIndent: 0,
+    );
+  }
+
+  Widget _hydration() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        const IntervalProgressBar(),
+        // _intervalBar(),
+        Text(
+          'Hidratación',
+          style: Theme.of(context).textTheme.bodySmall,
+        )
+      ],
+    );
+  }
+
+  // ignore: unused_element
+  Widget _intervalBar() {
+    return Text(
+      '1.0',
+      style: Theme.of(context).textTheme.headlineSmall,
+    );
+  }
+
+  Widget _buttonReg() {
+    return Center(
+        child: ConstrainedBox(
+      constraints: const BoxConstraints.tightFor(
+        width: 100,
+        height: 30,
+      ),
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 5,
+            ),
+            backgroundColor: const Color.fromARGB(255, 31, 15, 175),
+          ),
+          onPressed: () {},
+          child: const Text(
+            'Registrar',
+          )),
+    ));
   }
 }
